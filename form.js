c4b610ae8bd75e8ef2f0aed9f261a9064b6da901
tillyjay <97525044+tillyjay@users.noreply.github.com>
2023-02-10 11:34:57 -0400

    Add files via upload

---
+++ form.js
@@ -0,0 +1,14 @@
+
+// Form action functions 
+
+function formFocus(x) 
+{
+    x.style.border = "2px solid rgb(104, 195, 163)";
+    x.style.outline = "4px solid rgb(4, 147, 114)";
+}
+
+function formBlur(x) 
+{
+    x.style.border = "2px solid black";
+    x.style.outline = "none";
+}
